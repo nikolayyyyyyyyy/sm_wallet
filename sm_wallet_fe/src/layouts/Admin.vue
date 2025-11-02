afbe18f98ae75ed050b828d885ecad58773e3666
Nikolay Nikolaev <nikolay.nikolaev@touchpoint.bg>
2025-11-02 20:03:30 +0200

    Add listing pages for all tables

--- sm_wallet_fe/src/layouts/Admin.vue
+++ sm_wallet_fe/src/layouts/Admin.vue
@@ -5,26 +5,41 @@ import { RouterLink } from 'vue-router';
 
 <template>
     <section class="section__admin">
-        <div class="section__inner shell">
-            <div class="base-form admin-panel">
-                <RouterLink class="panel-item" to="/create-client">Добави Клиент</RouterLink>
-                <RouterLink class="panel-item" to="/create-currency">Добави Валута</RouterLink>
-                <RouterLink class="panel-item" to="create-account-type">Добави Тип Акаунт</RouterLink>
-                <RouterLink class="panel-item" to="create-transaction-type">Добави Тип Транзакция</RouterLink>
-                <RouterLink class="panel-item" to="create-account">Добави Карта</RouterLink>
-                <RouterLink class="panel-item" to="create-transaction">Добави Транзакция</RouterLink>
-            </div>
+        <div class="base-form admin-panel-create">
+            <RouterLink class="panel-item" to="/create-client">Добави Клиент</RouterLink>
+            <RouterLink class="panel-item" to="/create-currency">Добави Валута</RouterLink>
+            <RouterLink class="panel-item" to="/create-account-type">Добави Тип Акаунт</RouterLink>
+            <RouterLink class="panel-item" to="/create-transaction-type">Добави Тип Транзакция</RouterLink>
+            <RouterLink class="panel-item" to="/create-account">Добави Карта</RouterLink>
+            <RouterLink class="panel-item" to="/create-transaction">Добави Транзакция</RouterLink>
+        </div>
+
+        <div class="base-form admin-panel-get">
+            <RouterLink class="panel-item" to="/clients">Всички Клиенти</RouterLink>
+            <RouterLink class="panel-item" to="/currencies">Всички Валути</RouterLink>
+            <RouterLink class="panel-item" to="/account-types">Всички Типове Акаунти</RouterLink>
+            <RouterLink class="panel-item" to="/transaction-types">Всички Типове Транзакции</RouterLink>
+            <RouterLink class="panel-item" to="/accounts">Всички Карти</RouterLink>
+            <RouterLink class="panel-item" to="/transactions">Всички Транзакции</RouterLink>
         </div>
     </section>
 </template>
 
 <style scoped lang="scss">
-.admin-panel {
-    width: 30%;
-    margin: 100px auto;
+.section__admin {
+    margin-block: 32px;
     display: flex;
-    flex-direction: column;
-    gap: 12px;
+    justify-content: center;
+    gap: 50px;
+    flex-wrap: wrap;
+
+    .admin-panel-get,
+    .admin-panel-create {
+        width: 300px;
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+    }
 
     .panel-item {
         background-color: var(--c-white);
--- sm_wallet_fe/src/router.js
+++ sm_wallet_fe/src/router.js
@@ -3,12 +3,18 @@ import LoginPage from "./pages/auth/LoginPage.vue";
 import RegisterPage from "./pages/auth/RegisterPage.vue";
 import DashboardPage from "./pages/DashboardPage.vue";
 import ProfilePage from "./pages/auth/ProfilePage.vue";
-import CreateClientPage from "./pages/admin/CreateClientPage.vue";
-import CreateCurrencyPage from "./pages/admin/CreateCurrencyPage.vue";
-import CreateAccountTypePage from "./pages/admin/CreateAccountTypePage.vue";
-import CreateTransactionTypePage from "./pages/admin/CreateTransactionTypePage.vue";
-import CreateAccountPage from "./pages/admin/CreateAccountPage.vue";
-import CreateTransactionPage from "./pages/admin/CreateTransactionPage.vue";
+import CreateClientPage from "./pages/admin/CreateRecords/CreateClientPage.vue";
+import CreateCurrencyPage from "./pages/admin/CreateRecords/CreateCurrencyPage.vue";
+import CreateAccountTypePage from "./pages/admin/CreateRecords/CreateAccountTypePage.vue";
+import CreateTransactionTypePage from "./pages/admin/CreateRecords/CreateTransactionTypePage.vue";
+import CreateAccountPage from "./pages/admin/CreateRecords/CreateAccountPage.vue";
+import CreateTransactionPage from "./pages/admin/CreateRecords/CreateTransactionPage.vue";
+import AllClientsPage from "./pages/admin/AllRecordsListing/AllClientsPage.vue";
+import AllCurrenciesPage from "./pages/admin/AllRecordsListing/AllCurrenciesPage.vue";
+import AllAccountTypesPage from "./pages/admin/AllRecordsListing/AllAccountTypesPage.vue";
+import AllTransactionTypesPage from "./pages/admin/AllRecordsListing/AllTransactionTypesPage.vue";
+import AllAccountsPage from "./pages/admin/AllRecordsListing/AllAccountsPage.vue";
+import AllTransactionsPage from "./pages/admin/AllRecordsListing/AllTransactionsPage.vue";
 
 
 const routes = [
@@ -21,7 +27,13 @@ const routes = [
     { path: '/create-account-type', component: CreateAccountTypePage },
     { path: '/create-transaction-type', component: CreateTransactionTypePage },
     { path: '/create-account', component: CreateAccountPage },
-    { path: '/create-transaction', component: CreateTransactionPage }
+    { path: '/create-transaction', component: CreateTransactionPage },
+    { path: '/clients', component: AllClientsPage },
+    { path: '/currencies', component: AllCurrenciesPage },
+    { path: '/account-types', component: AllAccountTypesPage },
+    { path: '/transaction-types', component: AllTransactionTypesPage },
+    { path: '/accounts', component: AllAccountsPage },
+    { path: '/transactions', component: AllTransactionsPage },
 ]
 
 export const router = createRouter({
